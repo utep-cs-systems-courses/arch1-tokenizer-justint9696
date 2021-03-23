581c7b8631a5daa5d1ffd1f1f2ad77693ab9b81b
justint9696 <justin.tonkinson96@gmail.com>
2021-03-23 15:20:30 -0600

    progress on io

--- src/uimain.c
+++ src/uimain.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 #include "tokenizer.h"
 #include "history.h"
@@ -8,35 +8,40 @@ int main(void) {
   int running = 1;
   
   // Maximum input string is 32.
-  char input[32];
+  char *input;
 
   // Allocate memory for linked list.
   List *list = init_history();
   
-  printf("Enter a string or type 'help' for options.\n");
+  puts("Enter a string or 'o' for options.");
   
   while (running) {
     printf("> ");
 
     // Receive input from user and store in char *input.
-    scanf("%s", input);
+    gets(input);
 
     // Echo input back to user.
-    printf("%s\n", input);
+    puts(input);
     
-    if (!strcmp(input, "help")) {
+    if (input[0] == 'h' && input[1] == '\0') {
       // Show all available options to user.
-      printf("history - show all input history\nquit - quit the program\n");
-    } else if (!strcmp(input, "history")) {
+      puts("!n - show history item n\ns - show all input history\nq - quit the program");
+    } else if (input[0] == 's' && input[1] == '\0') {
       // Display all input history from user.
       print_history(list);
-    } else if (!strcmp(input, "quit")) {
+    } else if (input[0] == 'q' && input[1] == '\0') {
       // Free allocated memory within linked list.
       free_history(list);
       // Terminate the program.
       running = 0;
-    } else if (input[0] == '!' && strlen(input) > 1) {
-      // Cannot directly convert string to integer?
+    } else if (input[0] == '!' && input[1] == '\0') {
+      // Increase pointer by 1 since index 0 is '!'. 
+      input++;
+      // Convert string to int.
+      int id = atoi(input);
+      char *item = get_history(list, id);
+      if (item != NULL) puts(item);
     } else {
       // Add input to linked list.
       add_history(list, input);
